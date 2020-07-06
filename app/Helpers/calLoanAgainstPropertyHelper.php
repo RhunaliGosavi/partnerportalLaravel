85400a363c4e3854df35e3b18e77f481556f032a
Rhunali Gosavi <rhuna0606@gmail.com>
2020-07-07 04:41:25 +0530

    added validation fir personal loan and loan against property calculator

--- app/Helpers/calLoanAgainstPropertyHelper.php
+++ app/Helpers/calLoanAgainstPropertyHelper.php
@@ -1,16 +1,31 @@
 <?php
 namespace App\Helpers;
-
+use App\CalculatorPolicy;
 class calLoanAgainstPropertyHelper
 {
 
-   public function calLoanAgainstPropert($roi,$loanTenure,$monthlyIncome,$policyFOIR,$monthlyObligation,$expectedLoanAmount,$propertyValue,$policyLTV){
+   public function calLoanAgainstPropert($roi,$loanTenure,$monthlyIncome,$monthlyObligation,$expectedLoanAmount,$propertyValue){
+     $calData=CalculatorPolicy::first();
+     $error='';
+     $error.=empty($roi) ? 'Rate Of Interest' :'';
+     $error.=empty($monthlyIncome) ?  (!empty($error)? ',':'').' Monthly Income' :'';
+     $error.=empty($monthlyObligation) ? (!empty($error)? ',':'').' Obligation' : '';
+     $error.=empty($loanTenure) ? (!empty($error)? ',':'').' Loan Tenure' : '';
+     $error.=empty($expectedLoanAmount) ? (!empty($error)? ',':'').' Expected Loan Amount' : '';
+     $error.=empty($propertyValue) ? (!empty($error)? ',':'').' Property Value' : '';
+     $error.=empty($calData) ? (!empty($error)? ',':'').'FORI or LTV' : '';
+     $error.=!empty($error) ? ' Can not be empty' : '';
+     if(empty($error)){
+        $policyFOIR= $calData->loan_against_property_fori;
+        $policyLTV= $calData->loan_against_property_ltv;
         $loanAmt= round($this->getLoanAmount($roi,$loanTenure,$monthlyIncome,$policyFOIR,$monthlyObligation));
         $EMI=round($this->getEmi($roi,$loanTenure,$loanAmt));
         $desiredEmi=round($this->getDesiredEMI($roi,$loanTenure,$expectedLoanAmount));
         $desiredFOIR=round($this->getDesiredFOIR($policyFOIR,$monthlyObligation,$monthlyIncome,$roi,$loanTenure,$expectedLoanAmount));
         $desiredLtv=round($this->getDesiredLTV($expectedLoanAmount,$propertyValue,$policyLTV));
         return array('LoanAmt'=>$loanAmt,'EMI'=>$EMI,'DesiredEmi'=>$desiredEmi,'DesiredFOIR'=>$desiredFOIR,'DesiredLTV'=>$desiredLtv);
+     }
+     return array('error'=>$error);
 
    }
    public function getLoanAmount($roi,$loanTenure,$monthlyIncome,$policyFOIR,$monthlyObligation){
--- app/Helpers/calPersonalLoanHelper.php
+++ app/Helpers/calPersonalLoanHelper.php
@@ -2,19 +2,33 @@
 
 namespace App\Helpers;
 
+use App\CalculatorPolicy;
 
 class calPersonalLoanHelper
 {
     //$policyFOIR,$policyROI,$expectedROI in percentage
-    public function calculatePersonalLoan($monthlyIncome,$policyFOIR,$obligation,$policyROI,$loanTenure,$expectedROI) {
+    public function calculatePersonalLoan($monthlyIncome,$obligation,$loanTenure,$expectedROI) 
+    {
+        $calData=CalculatorPolicy::first();
+        $error='';
+        $error.=empty($monthlyIncome) ? 'Monthly income' :'';
+        $error.=empty($obligation) ? (!empty($error)? ',':'').' Obligation' : '';
+        $error.=empty($loanTenure) ? (!empty($error)? ',':'').' Loan Tenure' : '';
+        $error.=empty($expectedROI) ? (!empty($error)? ',':'').' Expected ROI' : '';
+        $error.=empty($calData) ? (!empty($error)? ',':'').' Personal loan FORI or ROI' : '';
+        $error.=!empty($error) ? ' Can not be empty' : '';
+       if(empty($error)){
+            $policyFOIR= $calData->personal_loan_fori;
+            $policyROI= $calData->personal_loan_roi;
+            $applicableLoanAmt=$this->getApplicableLoadAmt($monthlyIncome,$policyFOIR,$obligation,$policyROI,$loanTenure);
+            $applicableEMI=$this->getApplicableEmi($monthlyIncome,$policyFOIR,$obligation);
+            $desiredEMI=$this->getDesiredEMI($expectedROI,$loanTenure);
+            $desiredFOIR=round($this->getDesiredFOIR($expectedROI,$loanTenure,$obligation,$monthlyIncome,$policyFOIR));
+            $desiredROI= $this->getDesiredROI($expectedROI,$policyROI);
 
-        $applicableLoanAmt=$this->getApplicableLoadAmt($monthlyIncome,$policyFOIR,$obligation,$policyROI,$loanTenure);
-        $applicableEMI=$this->getApplicableEmi($monthlyIncome,$policyFOIR,$obligation);
-        $desiredEMI=$this->getDesiredEMI($expectedROI,$loanTenure);
-        $desiredFOIR=round($this->getDesiredFOIR($expectedROI,$loanTenure,$obligation,$monthlyIncome,$policyFOIR));
-        $desiredROI= $this->getDesiredROI($expectedROI,$policyROI);
-
-        return array('ApplicableAmt'=>$applicableLoanAmt,'ApplicableEMI'=>$applicableEMI,'DesiredEmai'=>$desiredEMI,'DesiredFOIR'=>$desiredFOIR,'DesiredROI'=>$desiredROI);
+            return array('ApplicableAmt'=>$applicableLoanAmt,'ApplicableEMI'=>$applicableEMI,'DesiredEmai'=>$desiredEMI,'DesiredFOIR'=>$desiredFOIR,'DesiredROI'=>$desiredROI);
+        }
+        return array('error'=>$error);
       
     } 
     public function getApplicableLoadAmt($monthlyIncome,$policyFOIR,$obligation,$policyROI,$loanTenure){
