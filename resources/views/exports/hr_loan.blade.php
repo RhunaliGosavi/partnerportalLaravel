90749cead0646b4c893b2ce4913ad09e42a105de
Priyanka Shelke <priyanka.shelke@neosofttech.com>
2020-07-16 12:49:40 +0530

    Added fields in refer friend & apply now template

--- resources/views/exports/hr_loan.blade.php
+++ resources/views/exports/hr_loan.blade.php
@@ -6,10 +6,9 @@
 			<th>Source Name</th>
 			<th>Source ID</th>
 			<th>Applicants Name</th>
-			<!-- <th>Employee Id</th> -->
 			<th>Mobile Number</th>
-			<th>Employee Designation</th>
-			<!-- <th>Email ID</th> -->
+			<th>Email ID</th>
+			<th>Loan Product</th>
 			<th>Loan Amount</th>
 			<th>Preferred time to contact</th>
 		</tr>
@@ -23,10 +22,9 @@
 				<td>@if(isset($loan->employee)){{$loan->employee->name}}@endif</td>
 				<td>{{$loan->employee_id}}</td>
 				<td>{{$loan->name}}</td>
-				<!-- <td>{{$loan->employee_id}}</td> -->
 				<td>{{$loan->mobile_number}}</td>
-				<td>{{$loan->designation}}</td>
-				<!-- <td>{{$loan->email}}</td> -->
+				<td>@if(isset($loan->employee)){{$loan->employee->email}}@endif</td>
+				<td>HR Loan</td>
 				<td>{{$loan->loan_amount}}</td>
 				<td>{{$loan->prefered_contact_time}}</td>
 			</tr>
