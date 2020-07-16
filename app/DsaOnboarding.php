2c942dc4f7c86e6054072bbaec17b09e7f187b06
Priyanka Shelke <priyanka.shelke@neosofttech.com>
2020-07-16 20:11:17 +0530

    Fixed softdelete issue

--- app/DsaOnboarding.php
+++ app/DsaOnboarding.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DsaOnboarding extends Model
 {
-
+    use SoftDeletes;
+    
     protected $table = 'dsa_onboarding';
 
     protected $fillable = [
--- app/ImportantLink.php
+++ app/ImportantLink.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ImportantLink extends Model
 {
-
+    use SoftDeletes;
+    
     protected $table = 'important_links';
 
     protected $fillable = [
