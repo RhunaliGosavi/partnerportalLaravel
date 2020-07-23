dfca15a2ee7370f8e50cca0ca9e1bfc3974ca855
Priyanka Shelke <priyanka.shelke@neosofttech.com>
2020-07-23 12:58:38 +0530

    Resolved seeder issue

--- database/seeds/DocumentChecklistProductSeeder.php
+++ database/seeds/DocumentChecklistProductSeeder.php
@@ -14,25 +14,25 @@ class DocumentChecklistProductSeeder extends Seeder
         DB::table('document_checklist_products')->insert(array(
             array(
               'id'=>1,
-              'sales_kit_product_id' => 1,
+              // 'sales_kit_product_id' => 1,
               'document_checklist_category_id'=>1,
-              'content_data'=>'• PAN Card <br>
-                • Address Proofs (Passport/Driving license/Aadhar card, etc.)<br>
-                • Identity Proofs (PAN card/Passport/Voter ID, etc.)<br>
-                • Basis Constitution - Partnership deed, MOA, AOA, Business Registration Certificate, GST certificate, TIN number, etc.<br>
-                • Applicant & Co-Applicant recent photographs',
+              'content_data'=>'<li>PAN Card</li>
+              <li>Address Proofs (Passport/Driving license/Aadhar card, etc.)</li>
+              <li>Identity Proofs (PAN card/Passport/Voter ID, etc.)</li>
+              <li>Basis Constitution - Partnership deed, MOA, AOA, Business Registration Certificate, GST certificate, TIN number, etc.</li>
+              <li>Applicant & Co-Applicant recent photographs</li>',
               'created_at'=>now(),
               'updated_at'=>now()
             ),
             array(
                 'id'=>2,
-                'sales_kit_product_id' => 2,
+                // 'sales_kit_product_id' => 2,
                 'document_checklist_category_id'=>2,
-                'content_data'=>'• PAN Card 2 <br>
-                  • Address Proofs (Passport/Driving license/Aadhar card, etc.) 2 <br>
-                  • Identity Proofs (PAN card/Passport/Voter ID, etc.) 2 <br>
-                  • Basis Constitution - Partnership deed, MOA, AOA, Business Registration Certificate, GST certificate, TIN number, etc.2 <br>
-                  • Applicant & Co-Applicant recent photograph 2',
+                'content_data'=>'<li>PAN Card</li>
+                <li>Address Proofs (Passport/Driving license/Aadhar card, etc.)</li>
+                <li>Identity Proofs (PAN card/Passport/Voter ID, etc.)</li>
+                <li>Basis Constitution - Partnership deed, MOA, AOA, Business Registration Certificate, GST certificate, TIN number, etc.</li>
+                <li>Applicant & Co-Applicant recent photographs</li>',
                 'created_at'=>now(),
                 'updated_at'=>now()
               ),
--- database/seeds/MarketingVisualSeeder.php
+++ database/seeds/MarketingVisualSeeder.php
@@ -16,25 +16,25 @@ class MarketingVisualSeeder extends Seeder
         DB::table('marketing_visuals')->insert(array(
             array(
               'id'=>1,
-              'loan_product_id' => 2,
+            //   'loan_product_id' => 2,
               'marketing_visual_category_id' => 1,
-              'file_path'=>'test.pdf',
+              'content_data'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eveniet earum.</p>',
               'created_at'=>now(),
               'updated_at'=>now()
             ),
             array(
                 'id'=>2,
-                'loan_product_id' => 1,
+                // 'loan_product_id' => 1,
                 'marketing_visual_category_id' => 3,
-                'file_path'=>'test1.pdf',
+                'content_data'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eveniet earum.</p>',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ),
             array(
                 'id'=>3,
-                'loan_product_id' => 3,
+                // 'loan_product_id' => 3,
                 'marketing_visual_category_id' => 2,
-                'file_path'=>'test12.pdf',
+                'content_data'=>'<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eveniet earum.</p>',
                 'created_at'=>now(),
                 'updated_at'=>now()
             ),
